4dd2d93c5fc4a05cef41f0525bdc99843c3d0475
revetteai <revetteai@gmail.com>
2025-05-15 09:02:18 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function (eleventyConfig) {
   // ✅ Copy static assets
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("robots.txt");
-  eleventyConfig.addPassthroughCopy("_redirects");
+
 
 
   // ✅ Blog post collection
